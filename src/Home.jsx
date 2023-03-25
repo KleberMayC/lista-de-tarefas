6d85a4bb4ff294afa9d1faf0fd27c34620206e0f
KleberMayC <klebercoelhogt@gmail.com>
2023-03-25 12:55:31 -0300

    projeto atualizado

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import Card2 from "./Card2";
 import Card3 from "./Card3";
 import Card4 from "./Card4";
 import "./Card.css";
-import "./Home.css";
+import "./home.css";
 import Logo from "./assets/logodnc.png";
 
 const Home = () => {
@@ -17,12 +17,15 @@ const Home = () => {
 
         <h1>Lista de Tarefas</h1>
       </nav>
-      <div className="Cards">
-        <Card1 />
+      <section className="container">
+        <div className="card">
+          <Card1 />
+        </div>
+
         <Card2 />
         <Card3 />
         <Card4 />
-      </div>
+      </section>
     </>
   );
 };
---
+++ src/View.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+
+import "./View.css";
+
+const View = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default View;
